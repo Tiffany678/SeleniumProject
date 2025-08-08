0bc523872ab246ef92da930a196d0a2dcc2acd50
Yingjie Yang <102939225+Tiffany678@users.noreply.github.com>
2025-08-08 13:42:59 -0400

    initial commit

---
+++ src/test/java/Pages/ProductsPages.java
@@ -0,0 +1,120 @@
+package Pages;
+
+import com.aventstack.extentreports.Status;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+
+import java.io.IOException;
+import java.time.Duration;
+
+import static Tests.SeleniumTest.driver;
+import static Tests.SeleniumTest.*;
+
+public class ProductsPages {
+    public static String formalShoes_xpath = "/html/body/div[2]/center/h2";
+    public static String sportsShoes_xpath = "/html/body/div[3]/center/h2";
+    public static String sneakersShoes_xpath = "/html/body/div[4]/center/h2";
+
+    public static String formalShoesDropdown_xpath = "/html/body/div[2]/center/div/i[1]";
+    public static String sportsShoesDropdown_xpath = "/html/body/div[3]/center/div/i[1]";
+    public static String sneakersShoesDropdown_xpath = "/html/body/div[4]/center/div/i[1]";
+
+    public static String formalShoes_firstShoeName = "/html/body/div[2]/table/tbody/tr[1]/td[1]";
+    public static String sportsShoes_firstShoeName = "/html/body/div[3]/table/tbody/tr[1]/td[1]";
+    public static String sneakersShoes_firstShoeName = "/html/body/div[4]/table/tbody/tr[1]/td[1]";
+
+    public static void formalShoes_verifyTitle()  {
+        String expectedTitle = "Formal Shoes";
+        String actualTitle = driver.findElement(By.xpath(formalShoes_xpath)).getText();
+        try {
+            Assert.assertEquals(expectedTitle, actualTitle);
+            test.log(Status.PASS, "First shoe name matches: " + actualTitle);
+          //  test.log(Status.PASS, test.addScreenCaptureFromPath(capture(driver))+"Test passed");
+        } catch (AssertionError e) {
+            test.log(Status.FAIL, "Assertion failed: Expected [" + expectedTitle + "] but found [" + actualTitle + "]");
+            throw e; // rethrow so the test fails in the test runner (like TestNG or JUnit)
+        }
+    }
+
+
+    public static void sportsShoes_verifyTitle() {
+        String expectedTitle = "Sports Shoes";
+        String actualTitle = driver.findElement(By.xpath(sportsShoes_xpath)).getText();
+        try {
+            Assert.assertEquals(expectedTitle, actualTitle);
+            test.log(Status.PASS, "First shoe name matches: " + actualTitle);
+        } catch (AssertionError e) {
+            test.log(Status.FAIL, "Assertion failed: Expected [" + expectedTitle + "] but found [" + actualTitle + "]");
+            throw e; // rethrow so the test fails in the test runner (like TestNG or JUnit)
+        }
+    }
+
+
+    public static void sneakersShoes_verifyTitle() {
+        String expectedTitle = "Sneakers";
+        String actualTitle = driver.findElement(By.xpath(sneakersShoes_xpath)).getText();
+        try {
+            Assert.assertEquals(expectedTitle, actualTitle);
+            test.log(Status.PASS, "First shoe name matches: " + actualTitle);
+        } catch (AssertionError e) {
+            test.log(Status.FAIL, "Assertion failed: Expected [" + expectedTitle + "] but found [" + actualTitle + "]");
+            throw e; // rethrow so the test fails in the test runner (like TestNG or JUnit)
+        }
+    }
+
+
+    public static void formalShoes_firstShoe_verify() {
+        String expectedFirstFormalShoes = "   Classic Cheltenham";
+        driver.findElement(By.xpath(formalShoesDropdown_xpath)).click();
+        String actualFirstFormalShoes = driver.findElement(By.xpath(formalShoes_firstShoeName)).getText();
+        try {
+            Assert.assertEquals(expectedFirstFormalShoes, actualFirstFormalShoes);
+            test.log(Status.PASS, "First shoe name matches: " + actualFirstFormalShoes);
+        } catch (AssertionError e) {
+            test.log(Status.FAIL, "Assertion failed: Expected [" + expectedFirstFormalShoes + "] but found [" + actualFirstFormalShoes + "]");
+            throw e; // rethrow so the test fails in the test runner (like TestNG or JUnit)
+        }
+    }
+
+    public static void sportsShoes_firstShoe_verify() {
+        String expectedFirstSportShoes = "   Ultimate";
+        driver.findElement(By.xpath(sportsShoesDropdown_xpath)).click();
+        String actualFirstSportShoe = driver.findElement(By.xpath(sportsShoes_firstShoeName)).getText();
+
+        try {
+            Assert.assertEquals(expectedFirstSportShoes, actualFirstSportShoe);
+            test.log(Status.PASS, "First shoe name matches: " + actualFirstSportShoe);
+        } catch (AssertionError e) {
+            test.log(Status.FAIL, "Assertion failed: Expected [" + expectedFirstSportShoes + "] but found [" + actualFirstSportShoe + "]");
+            throw e; // rethrow so the test fails in the test runner (like TestNG or JUnit)
+        }
+    }
+
+    public static void sneakersShoes_firstShoe_verify() {
+        String expectedFirstSneakerShoes = "   Archivo";
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement dropdown = wait.until(ExpectedConditions.elementToBeClickable(By.xpath(sneakersShoesDropdown_xpath)));
+
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", dropdown);
+        ((JavascriptExecutor) driver).executeScript("arguments[0].click();", dropdown);
+
+        String actualFirstSneakerShoes = driver.findElement(By.xpath(sneakersShoes_firstShoeName)).getText();
+
+        try {
+            Assert.assertEquals(expectedFirstSneakerShoes, actualFirstSneakerShoes);
+            test.log(Status.PASS, "First shoe name matches: " + actualFirstSneakerShoes);
+        } catch (AssertionError e) {
+            test.log(Status.FAIL, "Assertion failed: Expected [" + expectedFirstSneakerShoes + "] but found [" + actualFirstSneakerShoes + "]");
+            throw e; // rethrow so the test fails in the test runner (like TestNG or JUnit)
+        }
+    }
+
+
+}
---
+++ src/test/java/Tests/SeleniumTest.java
@@ -0,0 +1,75 @@
+package Tests;
+
+import Pages.HomePage;
+import Pages.ProductsPages;
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.io.File;
+import java.io.IOException;
+
+public class SeleniumTest {
+    public static WebDriver driver;
+    ExtentReports report;
+    static ExtentReports extent = new ExtentReports();
+    public static ExtentTest test;
+
+    @BeforeSuite
+    public static void setUp() throws InterruptedException {
+        driver = new ChromeDriver();
+        driver.get("https://anupdamoda.github.io/AceOnlineShoePortal/index.html");
+        ExtentSparkReporter spark = new ExtentSparkReporter("src/test/resources/reports/ExtentSparkReport.html");
+        extent.attachReporter(spark);
+        HomePage.click_hamburger_menu();
+        HomePage.click_onlineProducts_link();
+    }
+    public static String capture(WebDriver driver) throws IOException, IOException {
+        File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        File Dest = new File(System.getProperty("user.dir") + "/screenshots/" + System.currentTimeMillis() + ".png");
+        String errflpath = Dest.getAbsolutePath();
+        FileUtils.copyFile(scrFile,Dest );
+        return errflpath;
+    }
+
+    @Test
+    void ValidateTitles_onlineProducts() {
+        test = extent.createTest("Validate shoe Titles on Product page", "This test validates that the different shoe types are correct on products page");
+        ProductsPages.formalShoes_verifyTitle();
+        ProductsPages.sportsShoes_verifyTitle();
+        ProductsPages.sneakersShoes_verifyTitle();
+    //   extent.flush();
+    }
+
+    @Test
+    void validateFirstFormalShoes() {
+        test = extent.createTest("Validate First formal shoes on Product page", "This test validates first formal shoes on products page");
+        ProductsPages.formalShoes_firstShoe_verify();
+    }
+
+    @Test
+    void validateFirstSportShoes() {
+        test = extent.createTest("Validate First sport shoes on Product page", "This test validates first sport shoes on products page");
+        ProductsPages.sportsShoes_firstShoe_verify();
+    }
+
+    @Test
+    void validateFirstSneakerShoes() {
+        test = extent.createTest("Validate First sneaker shoes on Product page", "This test validates first sneaker shoes on products page");
+        ProductsPages.sneakersShoes_firstShoe_verify();
+    }
+
+    @AfterSuite
+    public static void cleanup() {
+        extent.flush();
+    }
+}

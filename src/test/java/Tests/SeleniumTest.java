093147a7f5090898dd97787029be85592c8c9db7
Yingjie Yang <102939225+Tiffany678@users.noreply.github.com>
2025-08-09 00:59:42 -0400

    add ci yml

--- src/test/java/Tests/SeleniumTest.java
+++ src/test/java/Tests/SeleniumTest.java
@@ -10,6 +10,7 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.BeforeTest;
@@ -26,13 +27,19 @@ public class SeleniumTest {
 
     @BeforeSuite
     public static void setUp() throws InterruptedException {
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new","--no-sandbox","--disable-dev-shm-usage","--window-size=1920,1080");
+
+        driver = new ChromeDriver(options);
         driver.get("https://anupdamoda.github.io/AceOnlineShoePortal/index.html");
+
         ExtentSparkReporter spark = new ExtentSparkReporter("docs/reports/ExtentSparkReport.html");
         extent.attachReporter(spark);
+
         HomePage.click_hamburger_menu();
         HomePage.click_onlineProducts_link();
     }
+
     public static String capture(WebDriver driver) throws IOException, IOException {
         File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
         File Dest = new File(System.getProperty("user.dir") + "/screenshots/" + System.currentTimeMillis() + ".png");
